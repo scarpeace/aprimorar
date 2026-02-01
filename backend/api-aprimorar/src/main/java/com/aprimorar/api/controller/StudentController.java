77adc307e8173cfc2dda700278e7515ac4a2fd8a
scarpeace <gustavo.scarpellini@gmail.com>
2026-02-01 20:21:35 -0300

    feat: adding documentation tags

--- backend/api-aprimorar/src/main/java/com/aprimorar/api/controller/StudentController.java
+++ backend/api-aprimorar/src/main/java/com/aprimorar/api/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.aprimorar.api.controller;
 import com.aprimorar.api.controller.dto.StudentReponseDto;
 import com.aprimorar.api.controller.dto.StudentRequestDto;
 import com.aprimorar.api.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
@@ -26,6 +27,7 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @Operation(summary = "List all STUDENTS", description = "Retrieves all students from database with pagination")
     @GetMapping()
     public ResponseEntity<Page<StudentReponseDto>> listStudents(
             @RequestParam(defaultValue = "0") int page,
@@ -40,6 +42,7 @@ public class StudentController {
         return ResponseEntity.ok(allStudents);
     }
 
+    @Operation(summary = "List all active STUDENTS", description = "Retrieves all ACTIVE students from database with pagination")
     @GetMapping("/active")
     public ResponseEntity<Page<StudentReponseDto>> listActiveStudents(
             @RequestParam(defaultValue = "0") int page,
@@ -54,6 +57,7 @@ public class StudentController {
 
     }
 
+    @Operation(summary = "List single STUDENT", description = "Retrieves single student based on ID")
     @GetMapping("/{studentId}")
     public ResponseEntity<StudentReponseDto> getStudentById(@PathVariable String studentId){
         var foundStudent = studentService.findById(studentId);
@@ -61,6 +65,7 @@ public class StudentController {
        return ResponseEntity.ok(foundStudent);
     }
 
+    @Operation(summary = "Create STUDENT", description = "Creates student with student, address and parent data")
     @PostMapping()
     public ResponseEntity<StudentReponseDto> createStudent(@RequestBody @Valid StudentRequestDto studentRequestDto){
         StudentReponseDto response = studentService.createStudent(studentRequestDto);
@@ -68,6 +73,7 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Delete STUDENT", description = "Soft delete single student based on ID")
     @DeleteMapping("/{studentId}")
     public ResponseEntity<String> deleteStudent(@PathVariable String studentId){
         studentService.softDeleteStudent(studentId);
@@ -75,6 +81,7 @@ public class StudentController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Update STUDENT", description = "Updates student with full student data")
     @PatchMapping("/{studentId}")
     public ResponseEntity<StudentReponseDto> updateStudent(
             @PathVariable String studentId ,
