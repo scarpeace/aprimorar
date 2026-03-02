e004481930eb599be87009ea56a4af50152114e2
scarpeace <gustavo.scarpellini@gmail.com>
2026-03-02 17:27:49 -0300

    fix: simplifying ui components

--- client/src/components/ui/label.tsx
+++ client/src/components/ui/label.tsx
@@ -1,19 +1,13 @@
-import * as React from "react"
 import { cn } from "@/lib/utils"
 
-const Label = React.forwardRef<
-  HTMLLabelElement,
-  React.LabelHTMLAttributes<HTMLLabelElement>
->(({ className, ...props }, ref) => (
-  <label
-    ref={ref}
-    className={cn(
-      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-      className
-    )}
-    {...props}
-  />
-))
-Label.displayName = "Label"
-
-export { Label }
+export function Label({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) {
+  return (
+    <label
+      className={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className
+      )}
+      {...props}
+    />
+  )
+}
