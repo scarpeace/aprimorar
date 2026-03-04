b6de7820dcd4b030820d7e9e033c218b963a1968
scarpeace <gustavo.scarpellini@gmail.com>
2026-03-04 10:43:41 -0300

    feat: implementing backend filters

--- server/api-aprimorar/src/main/java/com/aprimorar/api/repository/StudentRepository.java
+++ server/api-aprimorar/src/main/java/com/aprimorar/api/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.aprimorar.api.repository;
 
 import com.aprimorar.api.entity.Student;
+import com.aprimorar.api.enums.Activity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,22 @@ import java.util.UUID;
 public interface StudentRepository extends JpaRepository<Student, UUID>{
 
     Page<Student> findAllByActiveTrue(Pageable pageable);
+
+    Page<Student> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    Page<Student> findByActivity(Activity activity, Pageable pageable);
+
+    Page<Student> findByNameContainingIgnoreCaseAndActivity(String name, Activity activity, Pageable pageable);
+
+    Page<Student> findAllByActiveTrueAndNameContainingIgnoreCase(String name, Pageable pageable);
+
+    Page<Student> findAllByActiveTrueAndActivity(Activity activity, Pageable pageable);
+
+    Page<Student> findAllByActiveTrueAndNameContainingIgnoreCaseAndActivity(
+            String name,
+            Activity activity,
+            Pageable pageable
+    );
 
     Optional<Student> findByIdAndActiveTrue(UUID id);
 }
