06529aacfee4452bf187493d06fb347184650722
scarpeace <gustavo.scarpellini@gmail.com>
2026-03-22 22:18:09 -0300

    fixing git ignore

---
+++ client/src/gen/schemas/dashboard/index.ts
@@ -0,0 +1,4 @@
+export type { GetDashboardSummary200Schema, GetDashboardSummaryQueryParamsSchema, GetDashboardSummaryQueryResponseSchema } from "./getDashboardSummarySchema.ts";
+export { getDashboardSummary200Schema } from "./getDashboardSummarySchema.ts";
+export { getDashboardSummaryQueryParamsSchema } from "./getDashboardSummarySchema.ts";
+export { getDashboardSummaryQueryResponseSchema } from "./getDashboardSummarySchema.ts";
